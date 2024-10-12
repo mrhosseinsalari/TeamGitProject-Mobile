c52ffe6135b25f78aa1cc9a8b3e0ebbb4dcefd5a
Hossein Salari <hosein.salari81@gmail.com>
2024-10-12 20:11:46 +0330

    no blog found implemented

---
+++ app/src/main/java/com/example/teamgitproj/ui/widgets/HomeWidgets.kt
@@ -0,0 +1,95 @@
+package com.example.teamgitproj.ui.widgets
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Snackbar
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.example.teamgitproj.R
+import com.example.teamgitproj.data.model.Blog
+import com.example.teamgitproj.ui.theme.cError
+import com.example.teamgitproj.ui.theme.cText2
+import com.example.teamgitproj.ui.theme.cText5
+import com.example.teamgitproj.util.FadeInOutWidget
+import com.example.teamgitproj.util.NetworkChecker
+import kotlinx.coroutines.delay
+@Composable
+fun SnackBar(title: String) {
+    var isVisible by remember { mutableStateOf(false) }
+    LaunchedEffect(true) { isVisible = true }
+    FadeInOutWidget(isVisible) {
+        Snackbar(
+            modifier = Modifier.padding(16.dp),
+            backgroundColor = cError
+        ) {
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = title,
+                textAlign = TextAlign.Center
+            )
+        }
+        LaunchedEffect(true) {
+            delay(2500)
+            isVisible = false
+        }
+    }
+}
+@Composable
+fun HomeContent(data: List<Blog>, onRequestRefresh: () -> Unit) {
+    val context = LocalContext.current
+    if (!NetworkChecker(context).isInternetConnected)
+        SnackBar(title = "لطفا از اتصال دستگاه خود به اینترنت مطمئن شوید")
+    ConstraintLayout(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        val (noBlog , articleList) = createRefs()
+        if(data.isEmpty()) {
+            Column(modifier = Modifier
+                .constrainAs(noBlog) {
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+                .padding(bottom = 80.dp),
+                horizontalAlignment = Alignment.CenterHorizontally) {
+                Image(
+                    modifier = Modifier.size(120.dp),
+                    painter = painterResource(id = R.drawable.ic_no_article),
+                    contentDescription = null
+                )
+                Text(
+                    text = "مقاله ای برای نمایش وجود ندارد",
+                    style = MaterialTheme.typography.h5,
+                    color = cText2
+                )
+                TextButton(onClick = { onRequestRefresh.invoke() }) {
+                    Text(
+                        text = "بارگذاری مجدد",
+                        style = MaterialTheme.typography.caption,
+                        color = cText5,
+                    )
+                }
+            }
+        } else {
+        }
+    }
+}
