97567df989df67e2f643ce624fc082714bc90b0f
Hossein Salari <hosein.salari81@gmail.com>
2024-10-13 20:09:28 +0330

    fix: delete animation when user click on a blog

--- app/src/main/java/com/example/teamgitproj/ui/widgets/BlogWidgets.kt
+++ app/src/main/java/com/example/teamgitproj/ui/widgets/BlogWidgets.kt
@@ -2,6 +2,7 @@ package com.example.teamgitproj.ui.widgets
 
 import android.content.res.Configuration
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -10,6 +11,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
@@ -44,13 +46,17 @@ fun BlogList(
 
 @Composable
 fun Blog(blog: Blog, onClicked: (Blog) -> Unit) {
+    val interactionSource = remember { MutableInteractionSource() }
     val configuration = LocalConfiguration.current
     val context = LocalContext.current
     ConstraintLayout(
         modifier = Modifier
             .fillMaxSize()
             .padding(top = 8.dp, bottom = 8.dp)
-            .clickable {
+            .clickable(
+                interactionSource = interactionSource ,
+                indication = null
+            ) {
                 onClicked.invoke(blog)
             }
     ) {
