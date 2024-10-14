75aa8dd7378c9316be18a14db40de440ed1ce1c7
Hossein Salari <hosein.salari81@gmail.com>
2024-10-14 23:43:35 +0330

    feat: add blog content

--- app/src/main/java/com/example/teamgitproj/ui/features/BlogScreenUi.kt
+++ app/src/main/java/com/example/teamgitproj/ui/features/BlogScreenUi.kt
@@ -1,14 +1,19 @@
 package com.example.teamgitproj.ui.features
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import dev.burnoo.cokoin.navigation.getNavController
 import com.example.teamgitproj.data.model.Blog
+import com.example.teamgitproj.ui.widgets.BlogContent
 import com.example.teamgitproj.ui.widgets.BlogToolbar
 import com.example.teamgitproj.util.Cache
 import com.example.teamgitproj.util.KEY_BLOG
@@ -23,12 +28,22 @@ fun BlogScreenUi() {
     Scaffold(
         topBar = {
             BlogToolbar(
-                titleArticle = blog.title ,
-                onBackPressed = { navigation.popBackStack() } ,
+                titleArticle = blog.title,
+                onBackPressed = { navigation.popBackStack() },
                 onInfoClicked = { showInfoDialog = true }
             )
-        } ,
+        },
+        modifier = Modifier.fillMaxSize()
     ) {
+        Box(modifier = Modifier.fillMaxSize() , contentAlignment = Alignment.BottomCenter) {
+            BlogContent(blog) {
+                // navigation
+            }
 
+            if(showInfoDialog) {
+                // BlogInfoDialog()
+            }
+
+        }
     }
 }
