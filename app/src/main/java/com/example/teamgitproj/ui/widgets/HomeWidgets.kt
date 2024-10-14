f051125c0737ab962b5783bb3100e4ff121f0bd9
Hossein Salari <hosein.salari81@gmail.com>
2024-10-14 20:58:25 +0330

    fix: shzdeh kochoolo is dead, this is amirHossein Mohammadi

--- app/src/main/java/com/example/teamgitproj/ui/widgets/HomeWidgets.kt
+++ app/src/main/java/com/example/teamgitproj/ui/widgets/HomeWidgets.kt
@@ -183,7 +183,7 @@ fun HomeToolbar(
 @Composable
 fun DevelopersIds() {
     Column {
-        Developer(R.drawable.amir_mohammadi, "شازده کوچولو", "برنامه نویس اپ اندروید", "@dunijet")
+        Developer(R.drawable.amir_mohammadi, "امیر حسین محمدی", "برنامه نویس اپ اندروید", "@dunijet")
         Developer(R.drawable.omid_baharifar, "امید بهاری فر", "برنامه نویس پنل فرانت", "@weblax_ir")
         Developer(R.drawable.erfan_yousefi, "عرفان یوسفی", "برنامه نویس بک اند", "@erfanyousefi.ir")
         Developer(R.drawable.soroush_mosapoor, "سروش موسی\u200Cپور", "متخصص دِوآپس", "@codingcogs")
